14b51dd52c68b2dc855ab7afba85277760b624a8
Andrea Toso <andrea.toso99@gmail.com>
2018-12-12 15:15:09 +0100

    Create Xcode project

---
+++ client/BraintreeSwiftNodejs/BraintreeSwiftNodejs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BraintreeSwiftNodejs
+//
+//  Created by Andrea Toso on 12/12/2018.
+//  Copyright © 2018 Andrea Toso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
